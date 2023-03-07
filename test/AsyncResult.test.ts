5371fc143a0094c030aa8db80685be9f04aeb561
Nick Taylor <nick.taylor@tutamail.com>
2023-03-07 16:12:16 -0500

    remove unused import

--- test/AsyncResult.test.ts
+++ test/AsyncResult.test.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect, vi } from "vitest"
+import { describe, it, expect } from "vitest"
 import { AsyncResult } from "../src/AsyncResult"
 import { Async } from "../src/Async"
 import { Result } from "../src/Result"
