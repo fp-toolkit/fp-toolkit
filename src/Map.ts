390014d692e60fde512e01f08cce45099ed0514d
Nick Taylor <nick.taylor@tutamail.com>
2023-03-11 19:52:07 -0500

    a bunch of map tests

--- src/Map.ts
+++ src/Map.ts
@@ -16,6 +16,17 @@ const defaultOrderingComparer: OrderingComparer<never> = {
     equals: defaultEqualityComparer.equals,
 }
 
+/**
+ * Lookup a key/value pair (wrapped in a `Some`) from a `Map` using the given key.
+ * If the `Map` doesn't contain the key, returns `None`.
+ *
+ * Uses the given equality comparer if passed. Otherwise, defaults to reference
+ * equality (triple equals) for equality comparisons.
+ *
+ * @category Lookups
+ *
+ * @returns An `Option` containing a tuple of the key and value.
+ */
 const findWithKey =
     <K>(key: K, { equals }: EqualityComparer<K> = defaultEqualityComparer) =>
     <V>(map: ReadonlyMap<K, V>): Option<[K, V]> => {
@@ -32,11 +43,28 @@ const findWithKey =
         return Option.none
     }
 
+/**
+ * Test whether a `Map` contains the given key. Uses the given `EqualityComparer`
+ * if passed. Otherwise, defaults to reference equality (triple equals).
+ *
+ * @category Lookups
+ *
+ * @returns `true` if the key is in the `Map`, `false` otherwise.
+ */
 const containsKey =
     <K>(key: K, equalityComparer: EqualityComparer<K> = defaultEqualityComparer) =>
     <V>(map: ReadonlyMap<K, V>): boolean =>
         pipe(map, findWithKey(key, equalityComparer), Option.isSome)
 
+/**
+ * Get a value associated with the given key from the `Map`. Returns a `Some`
+ * containing the value, or `None` if the key is not in the `Map`.
+ *
+ * Uses the given equality comparer if passed, otherwise defaults to using
+ * reference equality (triple equals) for equality comparison.
+ *
+ * @category Lookups
+ */
 const find =
     <K>(key: K, equalityComparer: EqualityComparer<K> = defaultEqualityComparer) =>
     <V extends {}>(map: ReadonlyMap<K, V>): Option<V> =>
@@ -64,7 +92,7 @@ const add =
     ) =>
     (map: ReadonlyMap<K, V>): ReadonlyMap<K, V> => {
         if (map.size < 1) {
-            const out = new globalThis.Map<K, V>()
+            const out = empty<K, V>()
             out.set(key, value)
             return out
         }
@@ -87,14 +115,21 @@ const add =
         )
     }
 
+/**
+ * Make a new `Map` by producing a new each value for each key using
+ * the given function.
+ *
+ * @category Mapping
+ * @category Transformations
+ */
 const map =
     <K, V, R>(f: (k: K, v: V) => R) =>
     (map: ReadonlyMap<K, V>): ReadonlyMap<K, R> => {
         if (map.size < 1) {
-            return new globalThis.Map()
+            return empty<K, R>()
         }
 
-        const out = new globalThis.Map()
+        const out = empty<K, R>()
 
         for (const [k, v] of map) {
             out.set(k, f(k, v))
@@ -103,23 +138,44 @@ const map =
         return out
     }
 
+/**
+ * Get the first key for which the given predicate function returns
+ * true, wrapped in a `Some`. If no key is found, returns `None`. Uses
+ * the given `OrderingComparer` if passed, otherwise defaults to default
+ * ASCII-based sort.
+ *
+ * @category Lookups
+ */
 const findKey =
-    <K extends {}>(predicate: Predicate<K>) =>
-    <V>(map: ReadonlyMap<K, V>): Option<K> => {
-        for (const [k] of map) {
-            if (predicate(k)) {
-                return Option.some(k)
-            }
-        }
+    <K extends {}>(
+        predicate: Predicate<K>,
+        orderingComparer: OrderingComparer<K> = defaultOrderingComparer
+    ) =>
+    <V>(map: ReadonlyMap<K, V>): Option<K> =>
+        Option.ofNullish(keys(orderingComparer)(map).find(predicate))
 
-        return Option.none
-    }
-
+/**
+ * Creates a new empty map. Essentially an alias for `new globalThis.Map()`.
+ * Provided for convience to avoid having to use `globalThis`.
+ *
+ * @category Constructors
+ */
 const empty = <K = never, V = never>() => new globalThis.Map<K, V>()
 
+/**
+ * Returns `true` if at least one _value_ in the `Map` returns `true`
+ * for the given predicate function.
+ *
+ * @category Lookups
+ * @category Utils
+ */
 const exists =
     <V>(predicate: Predicate<V>) =>
     <K>(map: ReadonlyMap<K, V>): boolean => {
+        if (map.size < 1) {
+            return false
+        }
+
         for (const [, v] of map) {
             if (predicate(v)) {
                 return true
@@ -129,8 +185,21 @@ const exists =
         return false
     }
 
+/**
+ * Replace the value at a given key in the map using the given
+ * replacement function. Will use the given `EqualityComparer`
+ * if passed. Otherwise defaults to reference equality (triple equals).
+ *
+ * If the key isn't in the map, returns the map unchanged.
+ *
+ * @category Transformations
+ */
 const change =
-    <K, V>(key: K, f: (v: V) => V, equalityComparer: EqualityComparer<K>) =>
+    <K, V>(
+        key: K,
+        f: (v: V) => V,
+        equalityComparer: EqualityComparer<K> = defaultEqualityComparer
+    ) =>
     (map: ReadonlyMap<K, V>): ReadonlyMap<K, V> =>
         pipe(
             map,
@@ -254,18 +323,18 @@ const ofRecord = <K extends string, V>(
     )
 
 export const Map = {
-    exists, // docs and tests
+    exists,
     containsKey, // docs and tests
-    findWithKey, // docs and tests
-    find, // docs and tests
-    findKey, // docs and tests
+    findWithKey,
+    find,
+    findKey,
     add,
-    change, // docs and tests
+    change,
     map, // docs and tests
     filter, // docs and tests
     every, // docs and tests
     iter, // docs and tests
-    empty, // docs and tests
+    empty,
     size, // docs and tests
     isEmpty, // docs and tests
     reduce, // docs and tests
--- test/Map.test.ts
+++ test/Map.test.ts
@@ -1,5 +1,6 @@
 import { describe, it, expect } from "vitest"
 import { Map } from "../src/Map"
+import { Option } from "../src/Option"
 import { pipe } from "../src/composition"
 import { EqualityComparer, OrderingComparer } from "../src/prelude"
 
@@ -14,22 +15,22 @@ const cheeseByAgeComparer: OrderingComparer<Cheese> = {
 }
 
 const cheeseEqualityComparer: EqualityComparer<Cheese> = {
-    equals: (c1, c2) => c1.name === c2.name,
+    equals: (c1, c2) => c1.name === c2.name && c1.age === c2.age,
 }
 
 describe("Map", () => {
     describe("add", () => {
         it("adds a new key/value to an empty map", () => {
             expect(pipe(Map.empty(), Map.add(["Albus", 12]))).toStrictEqual(
-                new globalThis.Map([["Albus", 12]])
+                Map.ofArray([["Albus", 12]])
             )
         })
 
         it("adds a new key/value to a non-empty map without the same key using default equality", () => {
             expect(
-                pipe(new globalThis.Map([["Fido", 44]]), Map.add(["Albus", 12]))
+                pipe(Map.ofArray([["Fido", 44]]), Map.add(["Albus", 12]))
             ).toStrictEqual(
-                new globalThis.Map([
+                Map.ofArray([
                     ["Fido", 44],
                     ["Albus", 12],
                 ])
@@ -38,18 +39,18 @@ describe("Map", () => {
 
         it("overrides a key/value to a non-empty map with the same key using default equality", () => {
             expect(
-                pipe(new globalThis.Map([["Fido", 44]]), Map.add(["Fido", 12]))
-            ).toStrictEqual(new globalThis.Map([["Fido", 12]]))
+                pipe(Map.ofArray([["Fido", 44]]), Map.add(["Fido", 12]))
+            ).toStrictEqual(Map.ofArray([["Fido", 12]]))
         })
 
         it("adds a new key/value to a non-empty map without the same key using custom equality", () => {
             expect(
                 pipe(
-                    new globalThis.Map([[{ name: "Gouda", age: 2 }, "AA"]]),
+                    Map.ofArray([[{ name: "Gouda", age: 2 }, "AA"]]),
                     Map.add([{ name: "Cheddar", age: 0.5 }, "C"], cheeseEqualityComparer)
                 )
             ).toStrictEqual(
-                new globalThis.Map([
+                Map.ofArray([
                     [{ name: "Gouda", age: 2 }, "AA"],
                     [{ name: "Cheddar", age: 0.5 }, "C"],
                 ])
@@ -59,10 +60,337 @@ describe("Map", () => {
         it("adds a new key/value to a non-empty map with the same key using custom equality", () => {
             expect(
                 pipe(
-                    new globalThis.Map([[{ name: "Gouda", age: 2 }, "AA"]]),
+                    Map.ofArray([[{ name: "Gouda", age: 2 }, "AA"]]),
                     Map.add([{ name: "Gouda", age: 2 }, "B"], cheeseEqualityComparer)
                 )
-            ).toStrictEqual(new globalThis.Map([[{ name: "Gouda", age: 2 }, "B"]]))
+            ).toStrictEqual(Map.ofArray([[{ name: "Gouda", age: 2 }, "B"]]))
+        })
+    })
+
+    describe("findWithKey", () => {
+        it("returns None if the map is empty", () => {
+            expect(pipe(new globalThis.Map(), Map.findWithKey("Abby"))).toStrictEqual(
+                Option.none
+            )
+        })
+
+        it("returns None if the key is not in the Map (using default equality)", () => {
+            expect(
+                pipe(Map.ofArray([["Jared", 25]]), Map.findWithKey("Fubo"))
+            ).toStrictEqual(Option.none)
+        })
+
+        it("returns Some if the key is in the Map (using default equality)", () => {
+            expect(
+                pipe(Map.ofArray([["Jared", 25]]), Map.findWithKey("Jared"))
+            ).toStrictEqual(Option.some(["Jared", 25]))
+        })
+
+        it("returns None if the key is not in the Map (using custom equality)", () => {
+            expect(
+                pipe(
+                    Map.ofArray([[{ name: "Muenster", age: 4 }, 25]]),
+                    Map.findWithKey({ name: "Muenster", age: 3 }, cheeseEqualityComparer)
+                )
+            ).toStrictEqual(Option.none)
+        })
+
+        it("returns Some if the key is in the Map (using custom equality)", () => {
+            expect(
+                pipe(
+                    Map.ofArray([[{ name: "Muenster", age: 4 }, 25]]),
+                    Map.findWithKey({ name: "Muenster", age: 4 }, cheeseEqualityComparer)
+                )
+            ).toStrictEqual(Option.some([{ name: "Muenster", age: 4 }, 25]))
+        })
+    })
+
+    describe("containsKey", () => {
+        it.each([
+            [true, "e", "in"],
+            [false, "f", "not in"],
+        ])(
+            "returns %o if the key (%s) is %s the map (default equality)",
+            (expected, letter) => {
+                const letterCounts = Map.ofArray([
+                    ["a", 21],
+                    ["e", 17],
+                ])
+                expect(pipe(letterCounts, Map.containsKey(letter))).toBe(expected)
+            }
+        )
+
+        it.each([
+            [true, { name: "Provolone", age: 1.5 }, "in"],
+            [false, { name: "Brie", age: 0.5 }, "not in"],
+        ])(
+            "returns %o if the key (%s) is %s the map (custom equality)",
+            (expected, key) => {
+                const cheeseRankings = Map.ofArray([
+                    [{ name: "Provolone", age: 1.5 }, "A+"],
+                    [{ name: "Cheddar", age: 2 }, "B-"],
+                ])
+                expect(
+                    pipe(cheeseRankings, Map.containsKey(key, cheeseEqualityComparer))
+                ).toBe(expected)
+            }
+        )
+    })
+
+    describe("find", () => {
+        it.each([
+            [Option.some(17), "e", "in"],
+            [Option.none, "f", "not in"],
+        ])(
+            "returns the value (%o) if the key (%s) is %s the map (default equality)",
+            (expected, letter) => {
+                const letterCounts = Map.ofArray([
+                    ["a", 21],
+                    ["e", 17],
+                ])
+                expect(pipe(letterCounts, Map.find(letter))).toStrictEqual(expected)
+            }
+        )
+
+        it.each([
+            [Option.some("A+"), { name: "Provolone", age: 1.5 }, "in"],
+            [Option.none, { name: "Brie", age: 0.5 }, "not in"],
+        ])(
+            "returns the value (%o) if the key (%s) is %s the map (custom equality)",
+            (expected, key) => {
+                const cheeseRankings = Map.ofArray([
+                    [{ name: "Provolone", age: 1.5 }, "A+"],
+                    [{ name: "Cheddar", age: 2 }, "B-"],
+                ])
+                expect(
+                    pipe(cheeseRankings, Map.find(key, cheeseEqualityComparer))
+                ).toStrictEqual(expected)
+            }
+        )
+    })
+
+    describe("map", () => {
+        it("returns an empty map if given an empty map", () => {
+            expect(
+                pipe(
+                    Map.empty(),
+                    Map.map(() => 0)
+                )
+            ).toStrictEqual(Map.empty())
+        })
+
+        it("returns a new map containing mapped values", () => {
+            expect(
+                pipe(
+                    Map.ofArray([
+                        ["John", 23],
+                        ["George", 8],
+                        ["Jimmy", 88],
+                    ]),
+                    Map.map((name, age) => `${name}:${age - 3}`)
+                )
+            ).toStrictEqual(
+                Map.ofArray([
+                    ["John", "John:20"],
+                    ["George", "George:5"],
+                    ["Jimmy", "Jimmy:85"],
+                ])
+            )
+        })
+    })
+
+    describe("findKey", () => {
+        it("returns None for an empty map", () => {
+            expect(
+                pipe(
+                    Map.empty(),
+                    Map.findKey((n: number) => n === 20)
+                )
+            ).toStrictEqual(Option.none)
+        })
+
+        it("returns the first key for which the predicate returns true (default sort)", () => {
+            expect(
+                pipe(
+                    Map.ofArray([
+                        ["b", 2],
+                        ["d", 4],
+                        ["a", 1],
+                        ["c", 3],
+                    ]),
+                    Map.findKey(s => s.length === 1)
+                )
+            ).toStrictEqual(Option.some("a"))
+        })
+
+        it("returns None if no key matches the predicate (default sort)", () => {
+            expect(
+                pipe(
+                    Map.ofArray([
+                        ["b", 2],
+                        ["d", 4],
+                        ["a", 1],
+                        ["c", 3],
+                    ]),
+                    Map.findKey(s => s.length === 2)
+                )
+            ).toStrictEqual(Option.none)
+        })
+
+        it("returns the first key for which the predicate returns true (custom sort)", () => {
+            expect(
+                pipe(
+                    Map.ofArray([
+                        [{ name: "Mozzarella", age: 2 }, 2],
+                        [{ name: "Provolone", age: 1 }, 4],
+                        [{ name: "Provolone", age: 0.5 }, 1],
+                        [{ name: "Cheddar", age: 3 }, 3],
+                    ]),
+                    Map.findKey(
+                        cheese => cheese.name === "Provolone",
+                        cheeseByAgeComparer
+                    )
+                )
+            ).toStrictEqual(Option.some({ name: "Provolone", age: 0.5 }))
+        })
+
+        it("returns None if no key matches the predicate (custom sort)", () => {
+            expect(
+                pipe(
+                    Map.ofArray([
+                        [{ name: "Mozzarella", age: 2 }, 2],
+                        [{ name: "Provolone", age: 1 }, 4],
+                        [{ name: "Provolone", age: 0.5 }, 1],
+                        [{ name: "Cheddar", age: 3 }, 3],
+                    ]),
+                    Map.findKey(cheese => cheese.name === "Muenster", cheeseByAgeComparer)
+                )
+            ).toStrictEqual(Option.none)
+        })
+    })
+
+    describe("empty", () => {
+        it("returns an empty map", () => {
+            expect(Map.empty()).toStrictEqual(new globalThis.Map())
+        })
+    })
+
+    describe("exists", () => {
+        it("returns false for an empty map", () => {
+            expect(
+                pipe(
+                    Map.empty(),
+                    Map.exists(() => true)
+                )
+            ).toBe(false)
+        })
+
+        it("returns false if no value in the map matches the predicate", () => {
+            expect(
+                pipe(
+                    Map.ofArray([
+                        ["Cat", 1],
+                        ["Dog", 3],
+                        ["Mouse", 0],
+                    ]),
+                    Map.exists(n => n < 0)
+                )
+            ).toBe(false)
+        })
+
+        it("returns true if at least one value in the map matches the predicate", () => {
+            expect(
+                pipe(
+                    Map.ofArray([
+                        ["Cat", 1],
+                        ["Dog", 3],
+                        ["Mouse", 0],
+                    ]),
+                    Map.exists(n => n > 2)
+                )
+            ).toBe(true)
+        })
+    })
+
+    describe("change", () => {
+        describe("default equality", () => {
+            it("returns the map unchanged if the key doesn't exist", () => {
+                expect(
+                    pipe(
+                        Map.ofArray([
+                            ["Cat", 1],
+                            ["Dog", 2],
+                        ]),
+                        Map.change("Mouse", n => n + 1)
+                    )
+                ).toStrictEqual(
+                    Map.ofArray([
+                        ["Cat", 1],
+                        ["Dog", 2],
+                    ])
+                )
+            })
+
+            it("returns the map with changed binding if the key does exist", () => {
+                expect(
+                    pipe(
+                        Map.ofArray([
+                            ["Cat", 1],
+                            ["Dog", 2],
+                        ]),
+                        Map.change("Dog", n => n + 1)
+                    )
+                ).toStrictEqual(
+                    Map.ofArray([
+                        ["Cat", 1],
+                        ["Dog", 3],
+                    ])
+                )
+            })
+        })
+
+        describe("custom equality", () => {
+            it("returns the map unchanged if the key doesn't exist", () => {
+                expect(
+                    pipe(
+                        Map.ofArray([
+                            [{ name: "Cheddar", age: 1 }, 1],
+                            [{ name: "Mozzarella", age: 0.25 }, 2],
+                        ]),
+                        Map.change(
+                            { name: "Provolone", age: 0 },
+                            n => n + 1,
+                            cheeseEqualityComparer
+                        )
+                    )
+                ).toStrictEqual(
+                    Map.ofArray([
+                        [{ name: "Cheddar", age: 1 }, 1],
+                        [{ name: "Mozzarella", age: 0.25 }, 2],
+                    ])
+                )
+            })
+
+            it("returns the map with changed binding if the key does exist", () => {
+                expect(
+                    pipe(
+                        Map.ofArray([
+                            [{ name: "Cheddar", age: 1 }, 1],
+                            [{ name: "Mozzarella", age: 0.25 }, 2],
+                        ]),
+                        Map.change(
+                            { name: "Cheddar", age: 1 },
+                            n => n + 1,
+                            cheeseEqualityComparer
+                        )
+                    )
+                ).toStrictEqual(
+                    Map.ofArray([
+                        [{ name: "Cheddar", age: 1 }, 2],
+                        [{ name: "Mozzarella", age: 0.25 }, 2],
+                    ])
+                )
+            })
         })
     })
 })
